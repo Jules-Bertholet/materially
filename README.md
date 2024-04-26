1b3d331f2171e7ccb7dce56fb7cda26e7caa9c0d
Jules Bertholet <julesbertholet@quoi.xyz>
2024-04-25 23:14:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# `materially`
+
+[![Build Status](https://github.com/Jules-Bertholet/materially/actions/workflows/actions.yml/badge.svg)](https://github.com/Jules-Bertholet/materially/actions)
+[![API reference](https://img.shields.io/docsrs/materially)](https://docs.rs/materially/)
+[![Crates.io](https://img.shields.io/crates/v/materially)](https://crates.io/crates/materially)
+[![License](https://img.shields.io/crates/l/materially.svg)](https://github.com/Jules-Bertholet/materially#license)
+
+A macro for [material implication](https://simple.wikipedia.org/wiki/Implication_(logic)).
+
+`a => b` ("`a` implies `b`") means `!a || b`.
+
+## Examples
+
+```rust
+use materially::implies as i;
+
+assert!(i!(false => true));
+assert!(i!(false => false));
+assert!(i!(true => true));
+assert!(!i!(true => false));
+
+// Implication is right-associative
+assert!(i!(true => false => true => false));
+
+// let-chains style syntax is also supported
+assert!(i!(let Some(a) = Some(17) => a > 3 => let None = Some(17) => false));
+```
---
+++ src/lib.rs
@@ -0,0 +1,71 @@
+//! A macro for [material implication](https://simple.wikipedia.org/wiki/Implication_(logic)).
+//!
+//! `a => b` ("`a` implies `b`") means `!a || b`.
+//!
+//! # Examples
+//!
+//! ```rust
+//! use materially::implies as i;
+//!
+//! assert!(i!(false => true));
+//! assert!(i!(false => false));
+//! assert!(i!(true => true));
+//! assert!(!i!(true => false));
+//!
+//! // Implication is right-associative
+//! assert!(i!(true => false => true => false));
+//!
+//! // let-chains style syntax is also supported
+//! assert!(i!(let Some(a) = Some(17) => a > 3 => let None = Some(17) => false));
+//! ```
+
+#![warn(missing_docs)]
+
+/// The implication macro.
+/// See the crate-level documentation for examples.
+#[macro_export]
+macro_rules! implies {
+    (let $p:pat = $scrutinee:expr => $consequent:expr) => {
+        match $scrutinee {
+            $p => $consequent,
+            _ => true,
+        }
+    };
+    (let $p:pat = $scrutinee:expr => $($tail:tt)*) => {
+        $crate::implies!(let $p = $scrutinee => $crate::implies!($($tail)*))
+    };
+    ($antecedent:expr => $consequent:expr) => {
+        !$antecedent || $consequent
+    };
+    ($antecedent:expr => $($tail:tt)*) => {
+        $crate::implies!($antecedent => $crate::implies!($($tail)*))
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::implies as i;
+
+    #[test]
+    #[allow(clippy::assertions_on_constants)]
+    fn test() {
+        assert!(i!(false => true));
+        assert!(i!(false => false));
+        assert!(i!(true => true));
+        assert!(!i!(true => false));
+        assert!(i!(true => false => true => false));
+
+        for a in [false, true] {
+            for b in [false, true] {
+                for c in [false, true] {
+                    assert_eq!(i!(a => b => c), i!((a && b) => c));
+                }
+            }
+        }
+
+        assert!(i!(let Some(foo) = Some(0) => foo < 3));
+        assert!(!i!(let Some(foo) = Some(0) => foo > 3));
+
+        //assert!(i!(let Some(foo) = Some(0) => let _ = true));
+    }
+}
